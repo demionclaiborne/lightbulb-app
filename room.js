a9daffee95fd179028348adf9bb0a44ca6f1c915
DeMion Claiborne <demionclaiborne@gmail.com>
2016-11-17 18:17:16 -0800

    Updated Room object with array of lights turned on

--- room.js
+++ room.js
@@ -4,9 +4,17 @@ function Light() {
 
 function Room(numberOfLights) {
     this.lights = [];
+    this.lightsTurnedOn = [];
     this.changeLightStateEveryMultipleOf = function(multiple) {
+        this.lightsTurnedOn = [];
+        
         for (var j = multiple - 1; j < this.lights.length; j += multiple) {
-            this.lights[j].isOn = !this.lights[j].isOn;
+            var light = this.lights[j];
+            light.isOn = !light.isOn;
+
+            if (light.isOn) {
+                this.lightsTurnedOn.push(j + 1);
+            }
         }
     }
 
