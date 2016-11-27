45f8bbabf6ccd2808d35d0715dfabba197740aaa
DeMion Claiborne <demionclaiborne@gmail.com>
2016-11-27 12:52:55 -0800

    Added test for setNumberOfLights method

--- test/test.js
+++ test/test.js
@@ -1,25 +1,34 @@
 var assert = chai.assert;
 
 suite('Room', function() {
-    test('#changeLightStateEveryMultipleOf', function () {
+    test('#changeLightStateEveryMultipleOf', function() {
         var i = 0;
-        var x = new Room(20);
+        var room = new Room(20);
 
-        x.changeLightStateEveryMultipleOf(2);
+        room.changeLightStateEveryMultipleOf(2);
 
         for (i = 1 ; i < 20; i += 2) {
-            assert.equal(x.lights[i].isOn, true);
+            assert.equal(room.lights[i].isOn, true);
         }
 
-        x.changeLightStateEveryMultipleOf(3);
+        room.changeLightStateEveryMultipleOf(3);
 
         for (i = 2; i < 20; i += 3) {
             if ((i + 1) % 2 == 0) {
-                assert.equal(x.lights[i].isOn, false);
+                assert.equal(room.lights[i].isOn, false);
             }
             else {
-                assert.equal(x.lights[i].isOn, true);
+                assert.equal(room.lights[i].isOn, true);
             }
         }
     });
+    test('#setNumberOfLights', function() {
+        var room = new Room(5);
+
+        assert.equal(room.lights.length, 5);
+
+        room.setNumberOfLights(25);
+
+        assert.equal(room.lights.length, 25);
+    })
 });
